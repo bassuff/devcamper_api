9acbdfc0b0be54d8e83a2592281ab702a044296a
Axel Winter <axel.winter@kaiserkraft-europa.de>
2021-09-25 17:12:14 +0200

    Creating Routes & Responses In Express

--- server.js
+++ server.js
@@ -6,6 +6,37 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+// app.get('/', (req, res) => {
+//   // res.send('<h1>Hallo from Express</h1>');
+//   // res.json({name: 'Axel'});
+//   // res.status(400).json({success: false});
+//   res.status(200).json({success: true, data: {id: 1}});
+// });
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({success: true, msg: 'Show all bootcamps'});
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({success: true, msg: `Show bootcamp ${req.params.id}`});
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({success: true, msg: 'Create new bootcamp'});
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({success: true, msg: `Update bootcamp ${req.params.id}`});
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({success: true, msg: `Delete bootcamp ${req.params.id}`});
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
